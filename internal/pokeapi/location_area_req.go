dd6f6a421f71ccc067eec1599395c15a0df122a7
bmeek01101 <brandonmeek11@outlook.com>
2024-09-05 21:12:21 -0500

    add pokemon locations

--- internal/pokeapi/location_area_req.go
+++ internal/pokeapi/location_area_req.go
@@ -7,13 +7,9 @@ import (
 	"net/http"
 )
 
-func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
-	endpoint := "/location-area"
+func (c *Client) ListLocationAreas(locationAreaName *string) (LocationAreasResp, error) {
+	endpoint := "/location-area" + locationAreaName
 	fullURL := baseURL + endpoint
-
-	if pageURL != nil {
-		fullURL = *pageURL
-	}
 
 	dat, ok := c.cache.Get(fullURL)
 	if ok {
@@ -56,5 +52,57 @@ func (c *Client) ListLocationAreas(pageURL *string) (LocationAreasResp, error) {
 	c.cache.Add(fullURL, dat)
 
 	return locationAreasResp, nil
+
+}
+
+func (c *Client) GetLocationArea(locationAreaName string) (locationArea, error) {
+	endpoint := "/location-area"
+	fullURL := baseURL + endpoint
+
+	if pageURL != nil {
+		fullURL = *pageURL
+	}
+
+	dat, ok := c.cache.Get(fullURL)
+	if ok {
+		fmt.Println("cache hit!")
+		locationArea := LocationArea{}
+		err := json.Unmarshal(dat, &locationAreasResp)
+		if err != nil {
+			return LocationArea{}, err
+		}
+		return locationAreasResp, nil
+	}
+	fmt.Println("cache miss!")
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return LocationArea{}, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 399 {
+		return LocationArea{}, fmt.Errorf("bad status code: %v", resp.StatusCode)
+	}
+
+	dat, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	locationArea := LocationArea{}
+	err = json.Unmarshal(dat, &locationArea)
+	if err != nil {
+		return LocationArea{}, err
+	}
+
+	c.cache.Add(fullURL, dat)
+
+	return locationArea, nil
 
 }
